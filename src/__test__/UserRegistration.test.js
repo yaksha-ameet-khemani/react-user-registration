c6a47d689cadc59c8b504cb1bc21d207b2f40016
Ameet Khemani <ameettechademy@gmail.com>
2023-03-11 13:18:11 +0530

    user registration in react with test cases

---
+++ src/__test__/UserRegistration.test.js
@@ -0,0 +1,116 @@
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  queryByAttribute,
+  act,
+  waitFor,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import UserRegistrationForm from "src/components/UserRegistrationForm";
+
+const getById = queryByAttribute.bind(null, "id");
+
+const setup = () => {
+  const utils = render(<UserRegistrationForm />);
+  const name = getById(utils.container, "name");
+  const email = getById(utils.container, "email");
+  const contact = getById(utils.container, "contact");
+  const submit = getById(utils.container, "submit");
+
+  return {
+    name,
+    email,
+    contact,
+    submit,
+    ...utils,
+  };
+};
+
+let testName = "UserRegistration boundary";
+
+describe("boundary", () => {
+  test(testName + " name is required", async () => {
+    const { name } = setup();
+    act(() => {
+      fireEvent.blur(name);
+    });
+    await waitFor(async () => {
+      expect(await screen.findByText(/Name is required/i)).toBeTruthy();
+    });
+  });
+
+  test(testName + " name is Valid", async () => {
+    const { name } = setup();
+    act(() => {
+      fireEvent.blur(name);
+      fireEvent.change(name, { target: { value: "temp name" } });
+    });
+    await waitFor(async () => {
+      const nameError = screen.queryByText(/Name is required./i);
+      expect(nameError).toBeNull();
+    });
+  });
+});
+
+describe("boundary", () => {
+  test(testName + " Invalid Email", async () => {
+    const { email } = setup();
+    act(() => {
+      fireEvent.blur(email);
+    });
+    await waitFor(async () => {
+      expect(await screen.findByText(/Invalid Email/i)).toBeTruthy();
+    });
+  });
+
+  test(testName + " Email is invalid", async () => {
+    const { email } = setup();
+    act(() => {
+      fireEvent.blur(email);
+      fireEvent.change(email, { target: { value: "abc" } });
+    });
+    await waitFor(async () => {
+      expect(await screen.findByText(/Invalid Email/i)).toBeTruthy();
+    });
+  });
+
+  test(testName + " Email is Valid", async () => {
+    const { email } = setup();
+    act(() => {
+      fireEvent.blur(email);
+      fireEvent.change(email, { target: { value: "abc@def.com" } });
+    });
+    await waitFor(async () => {
+      const emailError = screen.queryByText(/Invalid Email/i);
+      console.debug(emailError);
+      expect(emailError).toBeNull();
+    });
+  });
+});
+
+describe("boundary", () => {
+  test("submit button disabled on invalid data", async () => {
+    const utils = render(<UserRegistrationForm />);
+    expect(getById(utils.container, "submit")).toBeInTheDocument();
+    expect(getById(utils.container, "submit")).toBeDisabled();
+  });
+
+  test("submit button enabled on valued text", async () => {
+    const utils = render(<UserRegistrationForm />);
+    const { name, email, contact } = setup();
+    await waitFor(() => {
+      fireEvent.blur(name);
+      fireEvent.change(name, { target: { value: "Tom Jerry" } });
+      fireEvent.blur(email);
+      fireEvent.change(email, { target: { value: "abc@mail.com" } });
+      fireEvent.blur(contact);
+      fireEvent.change(contact, { target: { value: "1234567890" } });
+      expect(getById(utils.container, "submit")).toBeInTheDocument();
+      expect(
+        getById(utils.container, "submit").getAttribute("disabled")
+      ).not.toBeTruthy();
+    });
+  });
+});
---
+++ src/components/UserRegistrationForm.js
@@ -0,0 +1,197 @@
+import { useEffect, useState } from "react";
+import "./UserRegistrationForm.css";
+
+const UserRegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    contact: "",
+  });
+  const [formValidData, setFormValidData] = useState({
+    validEmail: true,
+    validContact: true,
+  });
+
+  const [invalidForm, setInvalidForm] = useState(true);
+
+  const [formError, setFormError] = useState({
+    name: false,
+    email: false,
+    contact: false,
+  });
+
+  const blurHandler = (e) => {
+    if (e.target.value == "") {
+      setFormError((prev) => {
+        return {
+          ...prev,
+          [e.target.id]: true,
+        };
+      });
+    } else {
+      setFormError((prev) => {
+        return {
+          ...prev,
+          [e.target.id]: false,
+        };
+      });
+    }
+  };
+
+  //   if (!new RegExp(/^[0-9]{10}$/).test(e.target.value)) {
+  //     new RegExp(
+  //       /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  //     ).test(e.target.value)
+  const changeHandler = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      };
+    });
+    if (e.target.id === "email") {
+      if (
+        new RegExp(
+          /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        ).test(e.target.value)
+      ) {
+        setFormError((prev) => {
+          return {
+            ...prev,
+            email: false,
+          };
+        });
+      } else {
+        setFormError((prev) => {
+          return {
+            ...prev,
+            email: true,
+          };
+        });
+      }
+    }
+  };
+
+  const submitHandler = () => {
+    const isEmailValid = new RegExp(
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    ).test(formData.email);
+    if (isEmailValid) {
+      alert("User added");
+      setFormData({ name: "", email: "", contact: "" });
+      setInvalidForm(true);
+    } else {
+      setFormError((prev) => {
+        return {
+          ...prev,
+          email: true,
+        };
+      });
+    }
+  };
+
+  useEffect(() => {
+    const nameValue = formData.name;
+    const emailValue = formData.email;
+    const validEmail = new RegExp(
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    ).test(emailValue);
+    if (nameValue.length > 0 && validEmail) {
+      setInvalidForm(false);
+    } else {
+      setInvalidForm(true);
+    }
+  }, [formError]);
+
+  return (
+    <>
+      <table className="outer-table">
+        <thead>
+          <tr>
+            <td>
+              <h1>User Registration</h1>
+            </td>
+          </tr>
+        </thead>
+
+        <table className="inner-table">
+          <tr>
+            <td>*Name:</td>
+            <td>
+              <input
+                type="text"
+                id="name"
+                onBlur={blurHandler}
+                onChange={changeHandler}
+                value={formData.name}
+              ></input>
+              <br />
+              {formError.name && (
+                <span className="email-error" name="name-error">
+                  Name is required
+                </span>
+              )}
+            </td>
+          </tr>
+          <tr>
+            {" "}
+            <td>&nbsp;</td>{" "}
+          </tr>
+          <tr>
+            <td>*Email:</td>
+            <td>
+              <input
+                type="text"
+                id="email"
+                onBlur={blurHandler}
+                onChange={changeHandler}
+                value={formData.email}
+              ></input>
+              <br />
+              {formError.email && (
+                <span className="email-error" name="email-error">
+                  Invalid Email
+                </span>
+              )}
+            </td>
+          </tr>
+          <tr>
+            {" "}
+            <td>&nbsp;</td>{" "}
+          </tr>
+          <tr>
+            <td>Contact:</td>
+            <td>
+              <input
+                type="text"
+                id="contact"
+                onChange={changeHandler}
+                value={formData.contact}
+              ></input>
+            </td>
+          </tr>
+          <tr>
+            {" "}
+            <td>&nbsp;</td>{" "}
+          </tr>
+          <table>
+            <tr>
+              <td>
+                <input
+                  type="submit"
+                  name="submit"
+                  id="submit"
+                  value="Add User"
+                  disabled={invalidForm}
+                  onClick={submitHandler}
+                />
+              </td>
+            </tr>
+          </table>
+        </table>
+      </table>
+    </>
+  );
+};
+
+export default UserRegistrationForm;
